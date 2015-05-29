b8ce9c504049533b76188d2e03b67ee84756ee8c
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-05-29 14:13:47 +0200

    Añadido primera fase de estilo

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@ Word.create name: "Love"
 # Word.create name: "War"
 #Word.create name: "Death"
 
-Question.create ask: "Qué película se rodó en "
+Question.create ask: "¿Qué película se rodó en "
 #Question.create ask: "Qué película fue dirigida por "
 #Question.create ask: "En qué película aparece el actor "
 #Question.create ask: "Qué película pertenece al género "
-Question.create ask: "Qué película se llama "
+# Question.create ask: "Qué película se llama #{@filmCorrect.title}"
 
 Score.create puntuation: 0
